df32118675c07660848315b20957b57c7bf091ed
Eleasah Halsmer <eleasahjh@gmail.com>
2019-08-05 13:57:42 -0600

    Displaying Todo by passing props from app.js through todolist and todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
 
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
 
 const todos = [
   {
@@ -17,14 +21,15 @@ const todos = [
 class App extends React.Component {
   constructor(){
     super();
+    this.state = {
+      todos: todos
+    }
   }
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos}/>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,16 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+// This will be a functional component
+
+const TodoList = props => {
+  return (
+    <>
+      {props.todos.map(todo => (
+        <Todo task={todo.task}/>
+      ))}
+    </>
+  );
+};
+
+export default TodoList;
